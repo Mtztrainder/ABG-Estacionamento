2daf4c3e2806328e0a272ba6ed0b301d91a78dd0
brunomelo283 <bruno_alison12@hotmail.com>
2022-11-20 16:02:22 -0300

    Acesso

--- src/main/java/unoeste/fipp/andrebrunogabriel/abgestacionamentosfipp/abgestacionamento/Banco/Dal/AcessoDAL.java
+++ src/main/java/unoeste/fipp/andrebrunogabriel/abgestacionamentosfipp/abgestacionamento/Banco/Dal/AcessoDAL.java
@@ -1,9 +1,7 @@
 package unoeste.fipp.andrebrunogabriel.abgestacionamentosfipp.abgestacionamento.Banco.Dal;
 
 import unoeste.fipp.andrebrunogabriel.abgestacionamentosfipp.abgestacionamento.Banco.Util.Banco;
-import unoeste.fipp.andrebrunogabriel.abgestacionamentosfipp.abgestacionamento.Dados.Acesso;
-import unoeste.fipp.andrebrunogabriel.abgestacionamentosfipp.abgestacionamento.Dados.Marca;
-import unoeste.fipp.andrebrunogabriel.abgestacionamentosfipp.abgestacionamento.Dados.Veiculo;
+import unoeste.fipp.andrebrunogabriel.abgestacionamentosfipp.abgestacionamento.Dados.*;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -33,7 +31,7 @@ public class AcessoDAL{
         return false;
     }
 
-    public boolean RegistrarSaida(Veiculo veiculo, LocalDateTime dh) {
+    public boolean RegistrarSaida(Veiculo veiculo, LocalDateTime dh, double valor) {
         String sql ="update acesso " +
                     "set ac_horasaida = ?, " +
                         "ac_valor = ? " +
@@ -43,7 +41,7 @@ public class AcessoDAL{
             Banco.Conectar();
             PreparedStatement pstmt = Banco.getConn().prepareStatement(sql);
             pstmt.setString(1, dh.toString());
-            pstmt.setString(2, "");
+            pstmt.setDouble(2, valor);
             pstmt.setInt(3, veiculo.getId());
 
             if (!Banco.getConexao().manipular(pstmt.toString())) {
@@ -56,5 +54,125 @@ public class AcessoDAL{
             System.out.println("Erro: "+ sqlex.getMessage());
         }
         return false;
+    }
+
+
+
+    public Acesso Select(int id) {
+        Acesso ac = null;
+
+        try{
+            String sql = "select * from acesso ac inner join veiculo vec on ac.vei_cod = vec.vei_cod where ac_cod = ? ";
+            Banco.Conectar();
+            PreparedStatement pstmt = Banco.getConn().prepareStatement(sql);
+            pstmt.setInt(1, id);
+            ResultSet rs = Banco.getConexao().consultar(pstmt.toString());
+            rs.next();
+
+            ac = new Acesso(rs.getInt("ac_cod"),
+                    new VeiculoDAL().Select(rs.getInt("vei_cod")),
+                     rs.getTimestamp("ac_horaentrada").toLocalDateTime(),
+                    rs.getTimestamp("ac_horasaida").toLocalDateTime(),
+                     rs.getDouble("ac_valor"));
+
+        }catch(SQLException sqlex){
+            System.out.println("Erro: "+ sqlex.getMessage());
+        }
+        return ac;
+    }
+
+
+    public List<Acesso> Select() {
+        List<Acesso> listaAcesso = new ArrayList<Acesso>();
+        String sql = "SELECT * FROM ACESSO AC INNER JOIN VEICULO VEC" +
+                "ON AC.VEI_COD = VEC.VEI_COD" +
+                "INNER JOIN MODELO MO ON MO.MOD_COD = VEC.MOD_COD "+
+                "INNER JOIN MARCA MA ON MA.MAR_COD = MO.MAR_COD "+
+                "INNER JOIN PROPRIETARIO PR ON PR.PROP_COD = VEC.PROP_COD ";
+
+
+        Banco.Conectar();
+        ResultSet rs = Banco.getConexao().consultar(sql);
+        try {
+            while(rs.next())
+            {
+                listaAcesso.add(new Acesso(rs.getInt("ac_cod"),
+                        new Veiculo(rs.getInt("vei_cod"),
+                                rs.getString("vei_placa"),
+                                rs.getString("vei_cor"),
+                                new Modelo(rs.getInt("mod_cod"),
+                                        rs.getString("mod_desc"),
+                                        new Marca(rs.getInt("mar_cod"),
+                                                rs.getString("mar_desc"))
+                                ),
+                                new Proprietario(rs.getInt("prop_cod"),
+                                        rs.getString("prop_cpf"),
+                                        rs.getString("prop_nome"),
+                                        rs.getString("prop_email"),
+                                        rs.getInt("prop_cep"),
+                                        rs.getString("prop_uf"),
+                                        rs.getString("prop_cidade"),
+                                        rs.getString("prop_bairro"),
+                                        rs.getString("prop_rua"),
+                                        rs.getString("prop_numero"),
+                                        rs.getString("prop_fone")
+                                    )
+                                ),
+
+
+                                rs.getTimestamp("ac_horaentrada").toLocalDateTime(),
+                                rs.getTimestamp("ac_horasaida").toLocalDateTime(),
+                                rs.getDouble("ac_valor")));
+            }
+        }
+        catch (Exception e){}
+        return listaAcesso;
+    }
+
+
+    public List<Acesso> Select(String filtro) {
+        List<Acesso> listaAcesso = new ArrayList<Acesso>();
+
+        try {
+            String sql = "SELECT * FROM ACESSO AC INNER JOIN VEICULO VEC" +
+                    "ON AC.VEI_COD = VEC.VEI_COD" +
+                    "INNER JOIN MODELO MO ON MO.MOD_COD = VEC.MOD_COD "+
+                    "INNER JOIN MARCA MA ON MA.MAR_COD = MO.MAR_COD "+
+                    "INNER JOIN PROPRIETARIO PR ON PR.PROP_COD = VEC.PROP_COD WHERE LOWER(VEC.VEI_PLACA) LIKE ?";
+            Banco.Conectar();
+            PreparedStatement pstmt = Banco.getConn().prepareStatement(sql);
+            pstmt.setString(1, filtro.toLowerCase() + "%");
+            ResultSet rs = Banco.getConexao().consultar(pstmt.toString());
+            while (rs.next()) {
+                listaAcesso.add(new Acesso(rs.getInt("ac_cod"),
+                        new Veiculo(rs.getInt("vei_cod"),
+                                rs.getString("vei_placa"),
+                                rs.getString("vei_cor"),
+                                new Modelo(rs.getInt("mod_cod"),
+                                        rs.getString("mod_desc"),
+                                        new Marca(rs.getInt("mar_cod"),
+                                                rs.getString("mar_desc"))
+                                ),
+                                new Proprietario(rs.getInt("prop_cod"),
+                                        rs.getString("prop_cpf"),
+                                        rs.getString("prop_nome"),
+                                        rs.getString("prop_email"),
+                                        rs.getInt("prop_cep"),
+                                        rs.getString("prop_uf"),
+                                        rs.getString("prop_cidade"),
+                                        rs.getString("prop_bairro"),
+                                        rs.getString("prop_rua"),
+                                        rs.getString("prop_numero"),
+                                        rs.getString("prop_fone")
+                                )
+                        ),
+                        rs.getTimestamp("ac_horaentrada").toLocalDateTime(),
+                        rs.getTimestamp("ac_horasaida").toLocalDateTime(),
+                        rs.getDouble("ac_valor")));
+            }
+        } catch (SQLException sqlex) {
+            System.out.println("Erro: " + sqlex.getMessage());
+        }
+        return listaAcesso;
     }
 }
--- src/main/java/unoeste/fipp/andrebrunogabriel/abgestacionamentosfipp/abgestacionamento/Dados/Acesso.java
+++ src/main/java/unoeste/fipp/andrebrunogabriel/abgestacionamentosfipp/abgestacionamento/Dados/Acesso.java
@@ -1,4 +1,67 @@
 package unoeste.fipp.andrebrunogabriel.abgestacionamentosfipp.abgestacionamento.Dados;
 
+import java.time.LocalDateTime;
+
 public class Acesso {
+    private int Id;
+    private Veiculo Veiculo;
+    private LocalDateTime HoraDeEntrada;
+    private LocalDateTime HoraDeSaida;
+    private double Valor;
+
+    public Acesso() {
+        this(0, new Veiculo(), null,null, 0);
+    }
+
+    public Acesso(int id, Veiculo veiculo, LocalDateTime horaDeEntrada, LocalDateTime horaDeSaida, double valor) {
+        Id = id;
+        Veiculo = veiculo;
+        HoraDeEntrada = horaDeEntrada;
+        HoraDeSaida = horaDeSaida;
+        Valor = valor;
+    }
+
+    public Acesso(Veiculo veiculo, LocalDateTime horaDeEntrada, LocalDateTime horaDeSaida, double valor) {
+        this(0, veiculo, horaDeEntrada, horaDeSaida, valor);
+    }
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int id) {
+        Id = id;
+    }
+
+    public Veiculo getVeiculo() {
+        return Veiculo;
+    }
+
+    public void setVeiculo(Veiculo veiculo) {
+        Veiculo = veiculo;
+    }
+
+    public LocalDateTime getHoraDeEntrada() {
+        return HoraDeEntrada;
+    }
+
+    public void setHoraDeEntrada(LocalDateTime horaDeEntrada) {
+        HoraDeEntrada = horaDeEntrada;
+    }
+
+    public LocalDateTime getHoraDeSaida() {
+        return HoraDeSaida;
+    }
+
+    public void setHoraDeSaida(LocalDateTime horaDeSaida) {
+        HoraDeSaida = horaDeSaida;
+    }
+
+    public double getValor() {
+        return Valor;
+    }
+
+    public void setValor(double valor) {
+        Valor = valor;
+    }
 }
